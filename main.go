35d0d53e9349083c24cdae051163c63aae92e1a2
Vathsan <srivaths311@gmail.com>
2022-02-19 13:53:18 +0530

    Added New d2 command

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-func init(){
-	flag.StringVar(&token,"t", "", "Bot Token")
+func init() {
+	flag.StringVar(&token, "t", "", "Bot Token")
 	flag.Parse()
 }
 
@@ -24,14 +24,14 @@ var isPlaying bool = false
 
 func main() {
 
-	if token == ""{
+	if token == "" {
 		fmt.Println("Pass Token as Param")
 		return
 	}
-	
+
 	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + token)
-	if err != nil {	
+	if err != nil {
 		fmt.Println("Error creating Discord session: ", err)
 		return
 	}
@@ -54,7 +54,6 @@ func main() {
 	dg.Close()
 }
 
-
 func ready(s *discordgo.Session, event *discordgo.Ready) {
 	s.UpdateGameStatus(0, "!emo")
 }
@@ -63,7 +62,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Author.ID == s.State.User.ID {
 		return
-	}	
+	}
 
 	if strings.HasPrefix(m.Content, "!emo") {
 
@@ -83,10 +82,10 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		for _, vs := range g.VoiceStates {
 			if vs.UserID == m.Author.ID {
 				voiceLine := strings.Split(m.Content, " ")
-				if len(voiceLine) <=1 {
+				if len(voiceLine) <= 1 {
 					return
 				}
-				switch voiceLine[1]{
+				switch voiceLine[1] {
 				case "emo":
 					command = 1
 				case "theri":
@@ -103,13 +102,13 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 					command = 7
 				default:
 					return
-				} 
+				}
 
 				if isPlaying == false {
 					isPlaying = true
-					fmt.Printf("%s: %s -> %s",g.Name,m.Author,voiceLine[1])
+					fmt.Printf("%s: %s -> %s", g.Name, m.Author, voiceLine[1])
 					err = playSound(s, g.ID, vs.ChannelID)
-				}					
+				}
 				isPlaying = false
 
 				if err != nil {
@@ -119,9 +118,50 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			}
 		}
 	}
+
+	if strings.HasPrefix(m.Content, "!d2") {
+
+		c, err := s.State.Channel(m.ChannelID)
+		if err != nil {
+			return
+		}
+		g, err := s.State.Guild(c.GuildID)
+		if err != nil {
+			return
+		}
+		for _, vs := range g.VoiceStates {
+			if vs.UserID == m.Author.ID {
+				voiceLine := strings.Split(m.Content, " ")
+				if len(voiceLine) <= 1 {
+					return
+				}
+				switch voiceLine[1] {
+				case "ratata":
+					command = 51
+				case "die":
+					command = 52
+				default:
+					return
+				}
+
+				if isPlaying == false {
+					isPlaying = true
+					fmt.Printf("%s: %s -> %s", g.Name, m.Author, voiceLine[1])
+					err = playSound(s, g.ID, vs.ChannelID)
+				}
+				isPlaying = false
+
+				if err != nil {
+					fmt.Println("Error Playing sound:", err)
+				}
+				return
+			}
+		}
+
+	}
 }
 
-func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) () {
+func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
 
 	if event.Guild.Unavailable {
 		return
@@ -141,20 +181,31 @@ func loadSound() (buffer2 [][]uint8, err error) {
 	var filename string
 	var buffer = make([][]byte, 0)
 
-	switch command  {
-	case 1 : filename = "emodmg.dca"
-	case 2 : filename = "theri.dca"
-	case 3 : filename = "aiyayo.dca"
-	case 4 : filename = "siruthai.dca"
-	case 5 : filename = "wtf.dca"
-	case 6 : filename = "davara.dca"
-	case 7 : filename = "daedalus3.dca"
+	switch command {
+	case 1:
+		filename = "emodmg.dca"
+	case 2:
+		filename = "theri.dca"
+	case 3:
+		filename = "aiyayo.dca"
+	case 4:
+		filename = "siruthai.dca"
+	case 5:
+		filename = "wtf.dca"
+	case 6:
+		filename = "davara.dca"
+	case 7:
+		filename = "daedalus3.dca"
+	case 51:
+		filename = "ratata.dca"
+	case 52:
+		filename = "imightdie.dca"
 	}
 
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("Error opening dca file :", err)
-		return nil,err
+		return nil, err
 	}
 
 	var opuslen int16
@@ -167,14 +218,14 @@ func loadSound() (buffer2 [][]uint8, err error) {
 		if err == io.EOF || err == io.ErrUnexpectedEOF {
 			err := file.Close()
 			if err != nil {
-				return nil,err
+				return nil, err
 			}
-			return buffer,nil
+			return buffer, nil
 		}
 
 		if err != nil {
 			fmt.Println("Error reading from dca file :", err)
-			return nil,err
+			return nil, err
 		}
 
 		// Read encoded pcm from dca file.
@@ -184,7 +235,7 @@ func loadSound() (buffer2 [][]uint8, err error) {
 		// Should not be any end of file errors
 		if err != nil {
 			fmt.Println("Error reading from dca file :", err)
-			return nil,err
+			return nil, err
 		}
 		// Append encoded pcm data to the buffer.
 		buffer = append(buffer, InBuf)
@@ -194,7 +245,7 @@ func loadSound() (buffer2 [][]uint8, err error) {
 // playSound plays the current buffer to the provided channel.
 func playSound(s *discordgo.Session, guildID, channelID string) (err error) {
 
-	buffer,err2 := loadSound()
+	buffer, err2 := loadSound()
 
 	if err2 != nil {
 		return err
